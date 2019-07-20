13cd3663443d97523794dd7e9b2a6adcf5e310f2
Kostya <fedorrroff@gmail.com>
2019-07-20 13:46:24 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -24,7 +24,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 				Log.d("M_Bender", "CRITICAL")
 				status = Status.NORMAL
 				question = Question.NAME
-				return "Заебал, давай по новой\n${question.question}?" to status.color
+				return "Это неправильный ответ\n${question.question}" to status.color
 			}
 			status = status.nextStatus()
 			return "Это не правильный ответ!\n${question.question}" to status.color
