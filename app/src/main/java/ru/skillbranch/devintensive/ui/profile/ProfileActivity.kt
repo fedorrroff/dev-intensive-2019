3d7b2a5af87866de0775effc0fcc34e6bebaac63
Kostya <fedorrroff@gmail.com>
2019-08-05 12:12:33 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package ru.skillbranch.devintensive.ui.profile
 
 import android.graphics.Color
+import android.graphics.ColorFilter
 import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,21 +15,130 @@ import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 import android.view.inputmethod.EditorInfo
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 
 class ProfileActivity : AppCompatActivity() {
+
+    companion object {
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
+
+
+	private lateinit var viewModel: ProfileViewModel
+	var isEditMode = false
+    lateinit var viewFields: Map<String, TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_profile)
+        initViews(savedInstanceState)
+		initViewModel()
 
+    }
+
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields =  mapOf(
+            "nickName" to tv_nick_name,
+            "rank" to tv_rank,
+            "respect" to tv_respect,
+            "rating" to tv_rating,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository
+        )
+
+		isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+		showCurrentMode(isEditMode)
+
+		btn_edit.setOnClickListener(View.OnClickListener {
+			if (isEditMode) saveProfileInfo()
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        })
+
+		btn_switch_theme.setOnClickListener(View.OnClickListener {
+			viewModel.switchTheme()
+		})
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        for ((_,v) in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if(isEdit) 255 else 0
+        }
+
+		ic_eye.visibility = if(isEdit) View.GONE else View.VISIBLE
+		wr_about.isCounterEnabled = isEdit
+
+		with(btn_edit){
+			val filter: ColorFilter? = if(isEdit) {
+				PorterDuffColorFilter (
+					resources.getColor(R.color.color_accent, theme),
+					PorterDuff.Mode.SRC_IN
+				)
+			} else {
+				null
+			}
+
+			val icon = if (isEdit) {
+				resources.getDrawable(R.drawable.ic_save_black_24dp, theme)
+			} else {
+				resources.getDrawable(R.drawable.ic_edit_black_24dp, theme)
+			}
+
+			background.colorFilter = filter
+			setImageDrawable(icon)
+		}
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
+		outState?.putBoolean(IS_EDIT_MODE, isEditMode)
 
     }
+
+	private fun initViewModel() {
+		viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+		viewModel.getProfileData().observe(this, Observer { updateUI(it) })
+		viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+
+	}
+
+	private fun updateTheme(mode: Int) {
+		Log.d("M_ProfileActivity", "updateTheme")
+		delegate.setLocalNightMode(mode)
+	}
+
+	private fun updateUI(profile: Profile) {
+		profile.toMap().also {
+			for((k, v) in viewFields) {
+				v.text = it[k].toString()
+
+			}
+		}
+	}
+
+	private fun saveProfileInfo() {
+		Profile(
+			firstName = et_first_name.text.toString(),
+			lastName = et_last_name.text.toString(),
+			about = et_about.text.toString(),
+			repository = et_repository.text.toString()
+		).apply {
+			viewModel.saveProfileData(this)
+		}
+	}
 
 }
